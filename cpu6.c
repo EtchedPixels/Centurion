db1a1d9d04416ba966f97f6af313db1b0109dab3
Scott Mansell <phiren@gmail.com>
2022-07-06 22:51:19 +1200

    Check for interrupts before exec_pc
    Otherwise the first instruction of the interrupt shows up wrong
    in traces

--- cpu6.c
+++ cpu6.c
@@ -2380,9 +2380,9 @@ void cpu_deassert_irq(unsigned ipl) {
 
 unsigned cpu6_execute_one(unsigned trace)
 {
+	cpu6_interrupt(trace);
 	exec_pc = pc;
 
-	cpu6_interrupt(trace);
 	if (trace)
 		fprintf(stderr, "CPU %04X: ", pc);
 	op = fetch();
